f7aae1d2d99231bc91ab6eb79bd3c257cdb1592e
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-09 16:39:24 +0100

    feat dom / stile

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+
+const contenitore = document.createElement("div");
+document.body.appendChild(contenitore);
+
 for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0){
-        console.log("fizz")
-    } else if (i % 5 === 0){
-        console.log("buzz")
-    } else console.log(i)
+    const numBox = document.createElement("span");
+    numBox.textContent = `${i}`;
+    contenitore.append(numBox);
+    if (i % 3 === 0 && i % 5 === 0){
+        numBox.textContent = "FizzBuzz"
+        numBox.style.backgroundColor = "#0B132B"
+    }
+    else if (i % 3 === 0){
+        numBox.textContent = "Fizz"
+        numBox.style.backgroundColor = "#39A2AE"
+    } 
+    else if (i % 5 === 0){
+        numBox.textContent = "Buzz"
+        numBox.style.backgroundColor = "#561F37"
+    } 
+    else {
+    console.log(i)
+    numBox.style.backgroundColor = "#CBEF43"
+    }    
 }
+
